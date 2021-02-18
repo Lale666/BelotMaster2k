1f62fb162246686e6bf9157b7fa73da57dc5dbd6
Sinisa Lolic <79253038+Lale666@users.noreply.github.com>
2021-02-18 13:50:37 +0100

    Add files via upload
    V0.6

--- BelotMaster2k.ino
+++ BelotMaster2k.ino
@@ -1,32 +1,34 @@
-// Bela Master 2000 V0.5
-// 02.12.2020.
+// Bela Master 2000 V0.6
+// 09.12.2020.
 
 // #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
 
 LiquidCrystal_I2C lcd(0x27,16,2);
+
 const int SWITCH = 9;
 const int RGB1 = 3;
 const int RGB2 = 5;
 const int RGB3 = 6;
+const int DELAY = 500;
 int TURN = 0;
+const char *PRVI, *DRUGI, *TRECI, *CETVRTI;
+const char *P1 = "FAKI ";
+const char *P2 = "GELAS";
+const char *P3 = "KRAJA";
+const char *P4 = "LALE ";
 
 const byte ROWS = 4; // Four rows
 const byte COLS = 1; // Three columns
-// Define the Keymap
 char keys[ROWS][COLS] = {
   {'A'},
   {'B'},
   {'C'},
   {'D'}
 };
-// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
 byte rowPins[ROWS] = { 9, 8, 7, 4 };
-// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
 byte colPins[COLS] = { 2 }; 
-
-// Create the Keypad
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup() {
@@ -35,14 +37,70 @@ void setup() {
   pinMode (RGB3,OUTPUT); // green
   pinMode (SWITCH,INPUT_PULLUP);
   lcd.init();
-  lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("* LALE  MIJESA *");
+  lcd.clear();
+  lcd.print("   WELCOME TO   ");
   lcd.setCursor(1,1);
-  lcd.print("bela master 2k");
-  setColor(255,0,0);
+  lcd.print(" THE MACHINE!   ");
+  setColor(0,0,0);
   Serial.begin(9600);
+  delay(3000);
+
+  lcd.setCursor(0,0);
+  lcd.print("A) FAKI B) GELAS");
+  lcd.setCursor(0,1);
+  lcd.print("C) KRAJA D) LALE");
+  
+  while (TURN < 4) {
+    char key = kpd.getKey();
+    if(key) {
+    }
+    switch (key)
+    {
+      case 'A':
+      if(TURN==0) PRVI = P1;
+      if(TURN==1) DRUGI = P1;
+      if(TURN==2) TRECI = P1;
+      if(TURN==3) CETVRTI = P1;
+      setColor(255,0,255);
+      delay(DELAY);
+      setColor(0,0,0);
+      TURN++;
+      break;
+      case 'B':
+      if(TURN==0) PRVI = P2;
+      if(TURN==1) DRUGI = P2;
+      if(TURN==2) TRECI = P2;
+      if(TURN==3) CETVRTI = P2;
+      setColor(255,0,255);
+      delay(DELAY);
+      setColor(0,0,0);
+      TURN++;
+      break;
+      case 'C':
+      if(TURN==0) PRVI = P3;
+      if(TURN==1) DRUGI = P3;
+      if(TURN==2) TRECI = P3;
+      if(TURN==3) CETVRTI = P3;
+      setColor(255,0,255);
+      delay(DELAY);
+      setColor(0,0,0);
+      TURN++;
+      break;
+      case 'D':
+      if(TURN==0) PRVI = P4;
+      if(TURN==1) DRUGI = P4;
+      if(TURN==2) TRECI = P4;
+      if(TURN==3) CETVRTI = P4;
+      setColor(255,0,255);
+      delay(DELAY);
+      setColor(0,0,0);
+      TURN++;
+      break;
+  }
+ }
+  TURN = 0;
+  ispis(PRVI);
 }
 
 void loop() {
@@ -53,29 +111,17 @@ void loop() {
   {
     TURN++;
     if(TURN==1) {
-      lcd.setCursor(0,0);
-      lcd.print("* KRAJA MIJESA *");
-      setColor(0,255,0);
-      delay(666);
+      ispis(DRUGI);
     }
     if(TURN==2) {
-      lcd.setCursor(0,0);
-      lcd.print("* GELAS MIJESA *");
-      setColor(0,0,255);
-      delay(666);
+      ispis(TRECI);
     }
     if(TURN==3) {
-      lcd.setCursor(0,0);
-      lcd.print("* FAKI  MIJESA *");
-      setColor(255,0,255);
-      delay(666);
+      ispis(CETVRTI);
     }
     if(TURN==4) {
       TURN = 0;
-      lcd.setCursor(0,0);
-      lcd.print("* LALE  MIJESA *");
-      setColor(255,0,0);
-      delay(666);
+      ispis(PRVI);
     }
   }
   
@@ -85,25 +131,33 @@ void loop() {
   switch (key)
   {
     case 'A':
+      setColor(0,0,255);
       lcd.setCursor(0,1);
       lcd.print("ADUT JE: BUNDEVA");
+      delay(DELAY);
       break;
     case 'B':
+      setColor(255,0,0);
       lcd.setCursor(0,1);
       lcd.print("ADUT JE:  CRVENA");
+      delay(DELAY);
       break;
     case 'C':
+      setColor(0,255,0);
       lcd.setCursor(0,1);
       lcd.print("ADUT JE:  ZELENA");
+      delay(DELAY);
       break;
     case 'D':
+      setColor(255,255,0);
       lcd.setCursor(0,1);
-      lcd.print("ADUT JE:  ZIROVA");
+      lcd.print("ADUT JE:   ZIR  ");
+      delay(DELAY);
       break;
-    default:
-        Serial.println(key);
+//    default:
+//        Serial.println(key);
   }
-}
+ }
 
 void setColor(int red, int green, int blue) {
   red = 255 - red;
@@ -112,4 +166,17 @@ void setColor(int red, int green, int blue) {
   analogWrite(RGB1, red);
   analogWrite(RGB2, green);
   analogWrite(RGB3, blue);  
+}
+
+void ispis(const char *MIJESA) {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("* ");
+  lcd.print(MIJESA);
+  lcd.print(" MIJESA *");
+// lcd.setCursor(0,1);
+// lcd.print("* ");
+// lcd.print(MUS);
+// lcd.print(" JE MUS *");
+  delay(DELAY);
 }
