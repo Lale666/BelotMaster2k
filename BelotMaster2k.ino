dc9322ebb2148ae94fe63f6bec34bcb4e8a8aba2
Sinisa Lolic <79253038+Lale666@users.noreply.github.com>
2021-02-18 13:41:39 +0100

    Add files via upload
    V0.3

--- BelotMaster2k.ino
+++ BelotMaster2k.ino
@@ -1,39 +1,51 @@
-// Bela Master 2000 V0.2
-// 13.11.2020.
+// Bela Master 2000 V0.3
+// 18.11.2020.
 
-const int LED1 = 6;
-const int LED2 = 7;
-const int LED3 = 8;
-const int LED4 = 9;
-const int SWITCH = 5;
-int LED = 6;
+// #include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+const int SWITCH = 9;
+int TURN = 0;
 
 void setup() {
-
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(LED3,OUTPUT);
-  pinMode(LED4,OUTPUT);
-  pinMode(SWITCH,INPUT_PULLUP);
-  digitalWrite(LED,HIGH);
+  pinMode (SWITCH,INPUT_PULLUP);
+  lcd.init();
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(2,0);
+  lcd.print("LALE  MIJESA");
+  lcd.setCursor(1,1);
+  lcd.print("bela master 2k");
 }
 
 void loop() {
-
-  int ValueSW = digitalRead(SWITCH);
-  if(ValueSW)
-  {
+int ValueSW = digitalRead(SWITCH);
+  if(ValueSW) {
   }
   else
   {
-    digitalWrite(LED,LOW);
-    LED++;
-    if(LED==10)
-    {
-      LED = 6;
-      digitalWrite(LED,HIGH);
-    }    
-    digitalWrite(LED,HIGH);
-    delay(500);
+    TURN++;
+    if(TURN==1) {
+      lcd.setCursor(2,0);
+      lcd.print("KRAJA MIJESA");
+      delay(666);
+    }
+    if(TURN==2) {
+      lcd.setCursor(2,0);
+      lcd.print("GELAS MIJESA");
+      delay(666);
+    }
+    if(TURN==3) {
+      lcd.setCursor(2,0);
+      lcd.print("FAKI  MIJESA");
+      delay(666);
+    }
+    if(TURN==4) {
+      TURN = 0;
+      lcd.setCursor(2,0);
+      lcd.print("LALE  MIJESA");
+      delay(666);
+    }
   }
 }
